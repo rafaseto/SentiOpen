786019cb510745e0501c3d7c2771408b09c0b1b2
rafaseto <rafael08.0807@gmail.com>
2025-04-09 15:04:00 -0300

    update: sentiment_classification/utils def get_comments_by_issue_id()

--- src/sentiment_classification/utils.py
+++ src/sentiment_classification/utils.py
@@ -1,3 +1,22 @@
+def get_comments_by_issue_id(issue_id, cursor):
+    cursor.execute(
+        """
+        SELECT 
+            comment_id,
+            created_at,
+            body
+        FROM
+            issue_comments_from_release
+        WHERE 
+            issue_id = %s
+        ORDER BY
+            created_at;
+        """,(issue_id,)
+    )
+    comments = cursor.fetchall()
+
+    return comments
+
 def classify_comment_sentiment_gpt(comment_body, messages, client, model):
     prompt = f"Classify the sentiment of this comment: {comment_body}"
 
